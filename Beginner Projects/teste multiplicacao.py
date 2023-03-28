6ecdc963ddce78b40c9a028c39bac8ef1c0b291e
jessicabeatrizferreira <jessicabaf@gmail.com>
2023-03-28 07:19:17 -0300

    add teste de multiplicação em python

---
+++ Beginner Projects/teste multiplicacao.py
@@ -0,0 +1,8 @@
+num1 = float (input("Digite o primeiro numero:"))
+num2 = float (input("Digite o segundo numero:"))
+soma = num1 + num2
+mult = num1 * num2
+sub = num1 - num2
+div  = num1 / num2
+print("A soma é:", int(soma) , '\n', "A divisão é:", float (div), '\n' , "A subtração é:", int(sub), '\n', "A multiplicação é", int(mult))
+
